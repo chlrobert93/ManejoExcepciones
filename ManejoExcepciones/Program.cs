4e065052933db7c9dda52dc4176e47e4baeaeb1e
Eder <chlrobert93@gmail.com>
2022-02-27 17:06:07 -0600

    Add project files.

---
+++ ManejoExcepciones/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejoExcepciones
+{
+     class DivNumeros
+    {
+
+        int resultado;
+
+        DivNumeros()
+        {
+            resultado = 0;
+        }
+
+        //Crear un método
+        public void Division(int num1, int num2)
+        {
+            try
+            {
+                resultado = num1 / num2;
+            }
+
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine("Se ha producido una excepción del tipo: {0}", e);
+
+            }
+
+            finally
+            {
+                Console.WriteLine("El resultado final es: {0}", resultado);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+           DivNumeros divNumeros = new DivNumeros();
+            divNumeros.Division(21, 0);
+
+            Console.ReadKey();
+        }
+    }
+}
